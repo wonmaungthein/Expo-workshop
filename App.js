d18e1ce22ed14b202e4528bd1b6a8834273c05d3
Won Maung Thein <wonmaungthein@gmail.com>
2018-08-10 16:38:12 +0100

    Changed the Text

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hello Won Maung Thein, Welcome to Expo Workshop. Happy coding.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "orange"
+  }
+});
