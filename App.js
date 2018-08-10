e2638c80d80e38f3040754ca411c4fd65e6f6ea8
Won Maung Thein <wonmaungthein@gmail.com>
2018-08-10 16:49:04 +0100

    Added more line

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>
-          Hello Won Maung Thein, Welcome to Expo Workshop. Happy coding.
+          Hello Won Maung Thein, Welcome to Expo Workshop. Happy coding. What
+          are you going to do now?
         </Text>
       </View>
     );
